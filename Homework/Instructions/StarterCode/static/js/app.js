219f6ff447fce0d877d54cd0033cc568c32fd12f
Madison <madifisher15@yahoo.com>
2020-06-22 15:01:28 -0500

    update

--- Homework/Instructions/StarterCode/static/js/app.js
+++ Homework/Instructions/StarterCode/static/js/app.js
@@ -1,10 +1,48 @@
-// Fetch the JSON data and console log it.
+  // Fetch the JSON data and console log it.
 // Must create a server in Bash and use Incognito Window
 // in browser or Shft + Ctrl + R to refresh browser and ignore cache. 
-//( Day 2 Activity 3) 
 
-d3.json("samples.json").then(data=> {
+d3.json("samples.json").then((incomingData) => {
+    function otuPlot(id) {
     console.log(data)
+    }
+
+// Use filter() to pass the function as its argument
+var otuSamples = incomingData.filter(otuPlot);
+
+//Check to make sure you are filtering the OTU's
+console.log(otuSamples);
+
+// Use the map method with the arrow function to return all
+//the filtered OTU's
+var sample_values = otuSamples.map(samples => samples.name);
+
+// Use the map method with the arrow function to return all 
+//the filtered OTU metascores.
+var otu_ids = otuSamples.map(samples => samples.metascore);
+
+// Check your filtered metascores
+console.log(filteredSamples);
+
+//Create a trace
+var trace1 = {
+    x: sample_values,
+    y: otu_ids,
+    text: otu_labels,
+    type:"bar"
+};
+
+//Create the data array for the plot
+var data= [trace1];
+
+//Define the plot layout
+var layout = { 
+    title:"Top 10  Operational Taxonomic Units (OTUs)",
+    xaxis: {title: "Title"},
+    yaxis: {title: "Metascore OTU's"}
+}
+Plotly.newPlot("bar", data, layout);
+});
 
 
 //Promise Pending
@@ -28,22 +66,24 @@ d3.json("samples.json").then(data=> {
 
     // Get values from JSON response object to build the plots
     // Use 'sample_values' as the values for the bar chart
-    var sample_values = data.samples.sample_values;
+    //var sample_values = data.samples.filter(getPlot);
+        //console.log(getPlot);
     // Use 'otu_ids' as the labels for the bar chart
-    var otu_ids = data.samples.otu_ids; 
+    //var otu_ids = data.samples.otu_ids; 
     //Use 'otu_labels' as the hovertext for the chart
-    var otu_labels = data.samples.otu_labels;
+    //var otu_labels = data.samples.otu_labels;
 
     //Create a trace
-    var trace1 = {
-        x: sample_values,
-        y: otu_ids,
-        type:'bar'
-    };
+    //var trace1 = {
+        //x: sample_values,
+        //y: otu_ids,
+       // text: otu_labels,
+        //type:"bar"
+    //};
 
-    var data= [trace1];
-    var layout = { 
-        title:"Operational Taxonomic Units (OTUs)"
-    }
-    Plotly.newPlot("bar", data, layout);
-});
+    //var data= [trace1];
+    //var layout = { 
+        //title:"Top 10  Operational Taxonomic Units (OTUs)"
+    //}
+    //Plotly.newPlot("bar", data, layout);
+//});
