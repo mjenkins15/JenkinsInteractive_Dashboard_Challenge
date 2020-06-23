4afab57e4054eeb863a474fe80f307c0acb58d98
Madison <madifisher15@yahoo.com>
2020-06-23 13:11:05 -0500

    completed horizontal bar chart

--- Homework/Instructions/StarterCode/static/js/app.js
+++ Homework/Instructions/StarterCode/static/js/app.js
@@ -39,30 +39,38 @@
         var samples = incomingData.samples;
         console.log(samples);
         //Filters through samples 
-        var otuSamples = samples.filter(sample => sample.id==sampleid);
-        console.log(otuSamples);
+        var otuSamples = samples.filter(sample => sample.id==sampleid)[0];
+        console.log(otuSamples.otu_ids,"This is working");
         // Use the map method with the arrow function to return all the filtered OTU's
-        var sample_values = otuSamples.map(samples => samples.name);
+        //var sample_values = otuSamples.map(samples => samples.name);
+        //console.log(sample_values);
         // Use the map method with the arrow function to return all the filtered OTU metascores.
-        var otu_ids = otuSamples.map(samples => samples.metascore);
+        //var otu_ids = otuSamples.map(samples => samples.otu_ids);
+        // var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var otu_ids = otuSamples.otu_ids.slice(0,10).reverse();
+        var sample_values = otuSamples.sample_values.slice(0,10).reverse();
         // Check your filtered metascores
         console.log(otuSamples);
+        console.log(sample_values);
+        console.log(otu_ids);
+        
         //Create a trace
         var trace1 = {
             x: sample_values,
             y: otu_ids,
             //text: otu_labels,
-            type: "bar"
+            type: "bar",
+            orientation: "h",
         };
         //Create the data array for the plot
         var data = [trace1];
         //Define the plot layout
         var layout = {
             title: "Top 10  Operational Taxonomic Units (OTUs)",
-          //barmode: "group",
+            
+            barmode: "group",
             xaxis: { title: "Title" },
-            yaxis: { title: "Metascore OTU's"
-         }
+            yaxis: { title: "Metascore OTU's"}
         };
         //Plot the chart to a div tag with id "bar"
         Plotly.newPlot("bar", data, layout);
@@ -73,24 +81,24 @@ function buildMetadata(sampleid) {
 }
 function init() {
     var selector = d3.select("#selDataset");
-    d3.json("samples.json").then((data) => {
-        var sampleNames = data.names;
+    d3.json("samples.json").then((incomingData) => {
+        var sampleNames = incomingData.names;
         sampleNames.forEach((sample) => {
             selector
                 .append("option")
                 .text(sample)
                 .property("value", sample);
-        })
+        });
     var firstSample= sampleNames[0];
-    buildCharts(firstSample)
-    buildMetadata(firstSample)
-    })
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+    });
 }
 function optionChanged(sampleid) {
-    buildCharts(sampleid)
-    buildMetadata(sampleid)
+    buildCharts(sampleid);
+    buildMetadata(sampleid);
 }
-init()
+init();
 
 
 
